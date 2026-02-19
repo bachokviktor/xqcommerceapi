013b0d86985cc81bb7301e4c27da02fd2abaf4ca
Bachok Viktor <bachok.viktor@gmail.com>
2026-02-19 17:37:30 +0200

    Implement serializers for users app.

---
+++ users/serializers.py
@@ -0,0 +1,32 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+from django_countries.serializers import CountryFieldMixin
+
+
+class CreateUserSerializer(serializers.ModelSerializer):
+    """
+    This serializer is used only for user creation.
+    """
+    class Meta:
+        model = get_user_model()
+        fields = ["username", "password"]
+        extra_kwargs = {
+            "password": {"write_only": True}
+        }
+
+    def create(self, validated_data):
+        user = get_user_model()(username=validated_data["username"])
+        user.set_password(validated_data["password"])
+        user.save()
+
+        return user
+
+class UserSerializer(CountryFieldMixin, serializers.ModelSerializer):
+    """
+    User serializer.
+    Can be used to retrieve or update user data.
+    """
+    class Meta:
+        model = get_user_model()
+        fields = ["id", "username", "email", "first_name", "last_name",
+                  "bio", "profile_pic", "country"]
