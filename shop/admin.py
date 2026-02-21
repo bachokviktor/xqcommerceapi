b3b604b1adc6701252455249124b82305c8c6580
Bachok Viktor <bachok.viktor@gmail.com>
2026-02-21 14:40:35 +0200

    Add models to the shop app.

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
+from . import models
+
+
 # Register your models here.
+class ItemPhotoInline(admin.TabularInline):
+    model = models.ItemPhoto
+    extra = 1
+
+
+class ItemAdmin(admin.ModelAdmin):
+    inlines = [ItemPhotoInline]
+
+
+admin.site.register(models.Item, ItemAdmin)
+admin.site.register(models.ItemReview)
+admin.site.register(models.Cart)
--- shop/urls.py
+++ shop/urls.py
@@ -5,6 +5,6 @@ from . import views
 
 app_name = "shop"
 
-urlpatterns = {
+urlpatterns = [
     path("", views.empty, name="list_items"),
-}
+]
